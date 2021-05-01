86b76cb921a6b5c0f86e8eed2ca2a81990d3aec1
gaston <gastonmazzei95@gmail.com>
2021-05-01 12:35:26 -0300

    first commit

---
+++ call_methods_iteratively.py
@@ -0,0 +1,53 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+
+def main(name_1: str, name_2: str, name_3: str, name_4: str) -> None:
+	"""
+	Opens an image-type file with name equal to name_1, 2, 3, 4
+	Displays it with only one call to the axis object
+	"""
+
+	# Create the figure object
+	N,M = 2,2
+	f, ax = plt.subplots(N,M, figsize=(30,30))
+
+	# Iterate over the images
+	for i,x in enumerate([name_1, name_2, name_3, name_4]):
+
+		# Open the Image	
+		img = cv2.imread(x, cv2.IMREAD_GRAYSCALE)
+
+		#***************************************************************************************************
+		# WEIRD SNIPPET: list the methods, arguments, and both plotting and showing the title in one call***
+		#***************************************************************************************************
+		CMDS = [ 
+			[
+			 'imshow',		# method name 1
+			  [img],		# positional arguments 1
+			  {'cmap':'gray'}	# keyword arguments 1
+			 ],
+			 [
+			  'set_title',						# method name 2
+			  [f'image: "{x.split("/")[-1].split(".")[0]}"'],		# positional arguments 2
+			  {}							# keyword arguments 2
+			 ],
+			]
+		output = [_ for _ in map(lambda x: getattr(ax[i//2,i%2],x[0])(*x[1],**x[2]), CMDS)]
+
+
+	# Hide axes' axis and display
+	for i in range(N):
+		for j in range(M):
+			ax[i,j].axis('off')
+	plt.show()
+
+
+if __name__=='__main__':
+	try:
+		main(*sys.argv[1:])
+	except:
+		main('images/lena.png', 'images/face_grid.png', 'images/escher_print_gallery.png', 'images/little_cross.png')
+
